658d290cca1d448be3116ea911f7a60bd50d5a00
isagi ego <isagiego299@gmail.com>
2023-10-06 01:09:54 +0600

    day 1 done

---
+++ python/Day 1/day1.py
@@ -0,0 +1,36 @@
+# print or output something --> print( what to print )
+print("hello world!!")
+
+# string is a combination of char . the " " show the beginning and end of that string
+# to create a new line --> \n
+#New lines can be created with a backslash and n.
+print("hello\nworld")
+
+# string concatenate --> merch two or more string into one --> string + string
+#String Concatenation is done with the "+" sign
+print("hello" +" "+ "world")
+
+#indentation is very importent in python.without proper indentation, it will give indentation error
+
+# to comment a line in python --> #
+# to get input from user --> input(promt)
+# input() function always return string value
+# len() function to return the total number of char in a string
+print("hello " + (input("say something\n"))+"!!")
+print(len("hello"))
+
+# variable declere --> var_name = data
+a = input()
+print(a)
+
+#variable naming --> cannot use space. cannot use number at the beginning of var_name. cannot use any build-in-function name as var_name
+
+
+"""
+                _summary_
+        
+        to print somethin --> print(what you want to print)
+        create new line --> \n
+        to input --> input()
+"""
+
